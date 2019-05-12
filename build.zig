5fa0131843e4c02f0dc0f6c39cdf582d83c491d4
Luna <git@l4.pm>
2019-05-12 02:56:25 -0300

    initial source commit

---
+++ build.zig
@@ -0,0 +1,20 @@
+const Builder = @import("std").build.Builder;
+
+pub fn build(b: *Builder) void {
+    const mode = b.standardReleaseOptions();
+
+    const exe = b.addExecutable("dig", "src/main.zig");
+    exe.setBuildMode(mode);
+
+    const lib = b.addStaticLibrary("zigdig", "src/main.zig");
+    lib.setBuildMode(mode);
+
+    var main_tests = b.addTest("src/main.zig");
+    main_tests.setBuildMode(mode);
+
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&main_tests.step);
+
+    b.default_step.dependOn(&lib.step);
+    b.installArtifact(lib);
+}
