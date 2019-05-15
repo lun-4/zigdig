4bb33b97923dbe1f0ca0eda7b9fb71aa5c468ec9
Luna <git@l4.pm>
2019-05-15 20:47:01 -0300

    remove bad hack for "unitialized header" (not a problem)

--- src/packet.zig
+++ src/packet.zig
@@ -326,14 +326,7 @@ pub const DNSPacket = struct {
     }
 
     pub fn deserialize(self: *DNSPacket, deserializer: var) !void {
-        // assume id 0 is unitialized header
-        // and well, that DOES give us a 1 in 65535 chance of
-        // the library breaking.
-
-        // TODO: not rely on header id for header init-or-not
-        if (self.header.id == 0) {
-            self.header = try deserializer.deserialize(DNSHeader);
-        }
+        self.header = try deserializer.deserialize(DNSHeader);
 
         // allocate the slices based on header data (WHEN DESERIALIZING).
         // when serializing or using addQuestion we do a realloc.
